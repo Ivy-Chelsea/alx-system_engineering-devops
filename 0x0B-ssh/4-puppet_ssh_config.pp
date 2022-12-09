ef67671ccde5020473b87d37a6872cd2b067f77d
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-12-09 13:11:44 +0300

    configure

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,2 +1,12 @@
 # config ssh_config
 file_line {'pass auten No':
+  ensure => present,
+    path => '/etc/ssh/ssh_config',
+    line => ' PasswordAuthentication no',
+    match=> 'PasswordAuthentication yes',
+}
+file_line {'Add ident file':
+  ensure => present,
+    path => '/etc/ssh/ssh_config',
+    line => ' IdentityFile ~/.ssh/holberton',
+}
