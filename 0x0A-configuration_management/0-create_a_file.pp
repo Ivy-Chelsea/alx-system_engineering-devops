b06e2cbc024c0286485a49e555e4907138a70d8d
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-12-09 01:19:00 -0800

    create file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#create a file with a puppet
+file { 'create_file':
+    ensure  => 'file',
+    content => 'I love Puppet',
+    group   => 'www-data',
+    mode    => '0744',
+    owner   => 'www-data',
+    path    => '/tmp/holberton'
+}
