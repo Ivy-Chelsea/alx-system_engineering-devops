995693023d224de7acc438a6fffe3c7708b2205d
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2023-02-01 21:17:20 +0300

    recurse

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,7 +3,7 @@ import json
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None, ugly_flag=0):
+def recurse(subreddit, hot_list=[], after=None, ugly_flag=0]:
     user_agent = {"User-Agent": "unix:0-subs.py:v1.0"}
     if after is None:
         if ugly_flag == 1:
@@ -17,17 +17,17 @@ def recurse(subreddit, hot_list=[], after=None, ugly_flag=0):
         else:
             json_data = data.json().get("data").get("children", [])
             for post in json_data:
-                hot_list.append(post.get("data").get("title"))
+                hot_list.append(post.get("data").get("after")
             ugly_flag = 1
             after = data.json().get("data").get("after")
             return recurse(subreddit, hot_list, after, ugly_flag)
-    else:
-        data = requests.get("https://www.reddit.com/r/{}/hot/.json?after={}"
-                            .format(subreddit, after),
-                            headers=user_agent,
-                            allow_redirects=False)
-        json_data = data.json().get("data").get("children", [])
-        for post in json_data:
-                hot_list.append(post.get("data").get("title"))
-        after = data.json().get("data").get("after")
-        return recurse(subreddit, hot_list, after, ugly_flag)
+        else:
+            data = requests.get("https://www.reddit.com/r/{}/hot/.json?after={}"
+                                .format(subreddit, after),
+                                headers=user_agent,
+                                allow_redirects=False)
+            json_data = data.json().get("data").get("children", [])
+            for post in json_data:
+                    hot_list.append(post.get("data").get("title"))
+            after = data.json().get("data").get("after")
+            return recurse(subredit, hot_list, after, ugly_glag)
